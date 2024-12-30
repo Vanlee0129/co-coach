e77a13332bcc5d97ffebca615488d07b03b18aef
Vanlee0129 <vanlee0129@hotmail.com>
2024-12-31 01:11:47 +0800

    feat: 使用环境变量

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://ccjwedjrhxfbgtxzydfy.supabase.co';
-const supabaseKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNjandlZGpyaHhmYmd0eHp5ZGZ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU1MzE1MzcsImV4cCI6MjA1MTEwNzUzN30.Y4uNXeZwLDHLTxOgrQHx2RLLH-Pq3QXtCOV-5anz9B8';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || process.env.SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY || process.env.SUPABASE_KEY;
+
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
